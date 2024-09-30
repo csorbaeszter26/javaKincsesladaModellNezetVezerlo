b5944e09b54e3febbc98e4d131d973c0c903d3db
csorbaeszter26 <csorba.eszter.26@gmail.com>
2024-09-30 14:55:31 +0200

    oop modell megjelenites

---
+++ src/main/KincsesLada.java
@@ -0,0 +1,16 @@
+
+package main;
+
+import modell.Jatek;
+
+
+public class KincsesLada {
+
+    
+    public static void main(String[] args) {
+        new Jatek();
+        
+        //System.out.println(new Jatek().kezdes());
+    }
+    
+}
