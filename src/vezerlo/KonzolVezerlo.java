d7b56d9546bc5a365fee34629d344a859418d54c
csorbaeszter26 <csorba.eszter.26@gmail.com>
2024-09-30 15:42:32 +0200

    mvc konzol nezet

---
+++ src/vezerlo/KonzolVezerlo.java
@@ -0,0 +1,29 @@
+
+package vezerlo;
+
+import modell.JatekModell;
+import nezet.KonzolNezet;
+
+
+public class KonzolVezerlo {
+    
+    private JatekModell modell;
+    private KonzolNezet nezet;
+    
+    //peldanyositjuk ezta kettőt:
+    public KonzolVezerlo(JatekModell modell, KonzolNezet nezet) {
+        this.modell = modell;
+        this.nezet = nezet;
+        
+        start();
+    }
+    
+    public void start(){
+        nezet.megjelenit(modell.kezdes());
+        modell.setValasztas(nezet.bekeres());
+        nezet.megjelenit(modell.ellenorzes);
+        
+    }
+    
+    
+}
